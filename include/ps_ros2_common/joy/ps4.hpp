173e525e1db843a4dd429cddda6f7f6c7788a93f
Ar-Ray-code <ray255ar@gmail.com>
2021-03-04 01:35:25 +0900

    initial upload

---
+++ include/ps_ros2_common/joy/ps4.hpp
@@ -0,0 +1,55 @@
+#ifndef PS4_HPP
+#define PS4_HPP
+
+#include "ps_base.hpp"
+
+namespace ps4{
+class ps:public ps_base
+{
+    public:
+        void get_data(const sensor_msgs::msg::Joy::SharedPtr msg)
+        {
+            cross_btn       = msg->buttons[0];
+            circle_btn      = msg->buttons[1];
+            triangle_btn    = msg->buttons[2];
+            square_btn      = msg->buttons[3];
+
+            L1_btn          = msg->buttons[4];
+            R1_btn          = msg->buttons[5];
+            L2_btn          = msg->buttons[6];
+            R2_btn          = msg->buttons[7];
+
+            select_btn      = msg->buttons[8];
+            share_btn       = msg->buttons[8];
+            create_btn      = msg->buttons[8];
+
+            start_btn       = msg->buttons[9];
+            options_btn     = msg->buttons[9];
+            
+            PS_btn          = msg->buttons[10];
+
+            joy_left_x      = msg->axes[0];
+            joy_left_y      = msg->axes[1];
+
+            L2              = msg->axes[2];
+
+            joy_right_x     = msg->axes[3];
+            joy_right_y     = msg->axes[4];
+            
+            R2              = msg->axes[5];
+
+            d_pad_x         = msg->axes[6];
+            d_pad_y         = msg->axes[7];
+
+            d_pad2btn();
+        }
+
+        
+        // virtual void sub_joy_thread(const sensor_msgs::msg::Joy::SharedPtr msg)
+        // {
+        //     get_data(msg);
+        // }
+};
+}
+
+#endif
---
+++ include/ps_ros2_common/joy/ps_base.hpp
@@ -0,0 +1,130 @@
+#ifndef JOY_BASE_HPP
+#define JOY_BASE_HPP
+
+#define PS5 5
+#define PS4 4
+#define PS3 3
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include <string>
+
+class ps_base
+{        
+    public:
+        bool  up_btn;
+        bool  down_btn;
+        bool  right_btn;
+        bool  left_btn;
+        bool  square_btn;
+        bool  circle_btn;
+        bool  triangle_btn;
+        bool  cross_btn;
+        bool  L1_btn;
+        bool  R1_btn;
+        bool  select_btn;
+        bool  start_btn;
+        bool  PS_btn;
+        bool  share_btn;
+        bool  options_btn;
+        bool  create_btn;
+        bool  touch_pad;
+        bool  R2_btn;
+        bool  L2_btn;
+
+        float R2;
+        float L2;
+        float d_pad_x;
+        float d_pad_y;
+        float joy_left_x;
+        float joy_left_y;
+        float joy_right_x;
+        float joy_right_y;
+
+        virtual void get_data(const sensor_msgs::msg::Joy::SharedPtr msg)=0;
+        virtual void sub_joy_thread(const sensor_msgs::msg::Joy::SharedPtr msg)
+        {
+            get_data(msg);
+        };
+        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_joy;
+
+        ps_base()
+        {
+            init_data();
+        }
+
+        void btn2d_pad()
+        {
+            d_pad_x     = -1*float(right_btn)  + float(left_btn);
+            d_pad_y     = -1*float(down_btn)   + float(up_btn);  
+        }
+
+        void d_pad2btn()
+        {
+            if(int(d_pad_x) == 1)
+            {
+                left_btn = 1;
+                right_btn = 0;
+            }
+            else if(int(d_pad_x) == -1)
+            {
+                left_btn = 0;
+                right_btn = 1;
+            }
+            else
+            {
+                left_btn = 0;
+                right_btn = 0;
+            }
+
+            if(int(d_pad_y) == 1)
+            {
+                up_btn = 1;
+                down_btn = 0;
+            }
+            else if(int(d_pad_y) == -1)
+            {
+                up_btn = 0;
+                down_btn = 1;
+            }
+            else
+            {
+                up_btn = 0;
+                down_btn = 0;
+            }
+        }
+
+        void init_data()
+        {
+            up_btn      = 0;
+            down_btn    = 0;
+            right_btn   = 0;
+            left_btn    = 0;
+            square_btn  = 0;
+            circle_btn  = 0;
+            triangle_btn= 0;
+            cross_btn   = 0;
+            L1_btn      = 0;
+            R1_btn      = 0;
+            select_btn  = 0;
+            start_btn   = 0;
+            PS_btn      = 0;
+            share_btn   = 0;
+            options_btn = 0;
+            create_btn  = 0;
+            touch_pad   = 0;
+            R2_btn      = 0;
+            L2_btn      = 0;
+
+            R2          = 1.0;
+            L2          = 1.0;
+            d_pad_x     = 0;
+            d_pad_y     = 0;
+            joy_left_x  = 0;
+            joy_left_y  = 0;
+            joy_right_x = 0;
+            joy_right_y = 0;
+        }
+};
+
+#endif
